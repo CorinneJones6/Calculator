f0f3dcde1c63a80095ee03b949cbc64005d465d9
Corinne Jones <jonescorinne6@gmail.com>
2024-02-20 14:32:19 -0700

    Working parse class

--- cmdline.hpp
+++ cmdline.hpp
@@ -3,7 +3,7 @@
  * \brief Command line arguments handler for the ExpressionClasses project.
  *
  * Provides a utility function to process and utilize command line arguments passed to an application.
- * This includes parsing, validation, and possibly setting up initial parameters or configurations
+ * This includes parsing, validation, and setting up initial parameters or configurations
  * based on the arguments provided during the execution of the program.
  *
  * \author Corinne Jones
@@ -15,7 +15,20 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
+#include "Expr.hpp"
+#include "parse.hpp"
 
 using namespace std;
 
-void use_arguments(int argc, char **argv);
+typedef enum {
+
+  do_nothing,
+  do_interp,
+  do_print,
+  do_pretty_print,
+
+} run_mode_t;
+
+run_mode_t use_arguments(int argc, char **argv);
+
+
--- main.cpp
+++ main.cpp
@@ -7,12 +7,36 @@
 #include <iostream>
 #include "cmdline.hpp"
 #include "Expr.hpp"
+#include "parse.hpp"
 #include <string>
 #include <cstdlib>
 
 int main(int argc, char **argv) {
 
-  use_arguments(argc, argv);
+  run_mode_t type = use_arguments(argc, argv);
+    
+    switch(type) {
+        case do_nothing:
+            break;
+        case do_interp: {
+            Expr *e = parse(cin);
+            int i = e->interp();
+            cout << i << "\n";
+            break;
+        }
+        case do_print: {
+            Expr *e = parse(cin);
+            string str = e->to_string();
+            cout << str << "\n";
+            break;
+        }
+        case do_pretty_print: {
+            Expr *e = parse(cin);
+            string str = e->to_pretty_string();
+            cout << str << "\n";
+            break;
+        }
+    }
     
   cout << "All requirements completed, now exiting." << endl;
 
